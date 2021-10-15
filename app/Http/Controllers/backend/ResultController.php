2be87674ca4bae83dbd8ba3cf324b8b264e06301
wddyousuf <saikatyousuf@gmail.com>
2021-10-15 15:31:24 +0600

    Result

--- app/Http/Controllers/backend/ResultController.php
+++ app/Http/Controllers/backend/ResultController.php
@@ -39,38 +39,43 @@ class ResultController extends Controller
         $course=DB::table('courses')->where('year',$request->year)->where('semester',$request->semester)->where('session',$std->session)->count();
         for($i=1;$i<=$course;$i++){
             $count++;
-            $data->$a.$i=$request->$a.$count;
-            $data->$b.$i=$request->$c.$count;
-            if($request->$a.$count>=80 && $request->$a.$count<=100){
-                $data->$d.$count=4.00;
-                $cigi=$cigi+$request->$c.$count*4.00;
-            }elseif($request->$a.$count>=75 && $request->$a.$count<80){
-                $data->$d.$count=3.75;
-                $cigi=$cigi+$request->$c.$count*3.75;
-            }elseif($request->$a.$count>=70 && $request->$a.$count<75){
-                $data->$d.$count=3.50;
-                $cigi=$cigi+$request->$c.$count*3.50;
-            }elseif($request->$a.$count>=65 && $request->$a.$count<70){
-                $data->$d.$count=3.25;
-                $cigi=$cigi+$request->$c.$count*3.25;
-            }elseif($request->$a.$count>=60 && $request->$a.$count<65){
-                $data->$d.$count=3.00;
-                $cigi=$cigi+$request->$c.$count*3.00;
-            }elseif($request->$a.$count>=55 && $request->$a.$count<60){
-                $data->$d.$count=2.75;
-                $cigi=$cigi+$request->$c.$count*2.75;
-            }elseif($request->$a.$count>=50 && $request->$a.$count<55){
-                $data->$d.$count=2.50;
-                $cigi=$cigi+$request->$c.$count*2.50;
-            }elseif($request->$a.$count>=45 && $request->$a.$count<50){
-                $data->$d.$count=2.25;
-                $cigi=$cigi+$request->$c.$count*2.25;
-            }elseif($request->$a.$count>=40 && $request->$a.$count<45){
-                $data->$d.$count=2.00;
-                $cigi=$cigi+$request->$c.$count*2.00;
+            $mark=$a.$count;
+            $dmark=$a.$i;
+            $crdt=$c.$count;
+            $dcrdt=$b.$i;
+            $grade=$d.$count;
+            $data->$dmark=$request->$mark;
+            $data->$dcrdt=$request->$crdt;
+            if($request->$mark>=80 && $request->$mark<=100){
+                $data->$grade=4.00;
+                $cigi=$cigi+$request->$crdt*4.00;
+            }elseif($request->$mark>=75 && $request->$mark<80){
+                $data->$grade=3.75;
+                $cigi=$cigi+$request->$crdt*3.75;
+            }elseif($request->$mark>=70 && $request->$mark<75){
+                $data->$grade=3.50;
+                $cigi=$cigi+$request->$crdt*3.50;
+            }elseif($request->$mark>=65 && $request->$mark<70){
+                $data->$grade=3.25;
+                $cigi=$cigi+$request->$crdt*3.25;
+            }elseif($request->$mark>=60 && $request->$mark<65){
+                $data->$grade=3.00;
+                $cigi=$cigi+$request->$crdt*3.00;
+            }elseif($request->$mark>=55 && $request->$mark<60){
+                $data->$grade=2.75;
+                $cigi=$cigi+$request->$crdt*2.75;
+            }elseif($request->$mark>=50 && $request->$mark<55){
+                $data->$grade=2.50;
+                $cigi=$cigi+$request->$crdt*2.50;
+            }elseif($request->$mark>=45 && $request->$mark<50){
+                $data->$grade=2.25;
+                $cigi=$cigi+$request->$crdt*2.25;
+            }elseif($request->$mark>=40 && $request->$mark<45){
+                $data->$grade=2.00;
+                $cigi=$cigi+$request->$crdt*2.00;
             }else{
-                $data->$d.$count=0.00;
-                $cigi=$cigi+$request->$c.$count*0.00;
+                $data->$grade=0.00;
+                $cigi=$cigi+$request->$crdt*0.00;
             }
 
 
